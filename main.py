f99c3b280f3b59c03057df437b430dabeb46208f
Andrew Stromme <andrew.stromme@gmail.com>
2017-11-11 11:34:57 -0800

    fix typo

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def run():
                      labels_tensor, keep_prob_tensor, learning_rate, debug_ops, merged, train_writer, test_writer)
 
         # TODO: Save inference data using helper.save_inference_samples
-        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_tensor)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob_tensor, input_tensor)
 
         # OPTIONAL: Apply the trained model to a video
 
